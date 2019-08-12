d6296454ae56bd02e82dd28ff8042ed626a7f0c8
Sivadass N <nsivadass@gmail.com>
2019-08-12 23:56:19 +0530

    Added  API key as env var

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -1,9 +1,9 @@
-// using Twilio SendGrid's v3 Node.js Library
-// https://github.com/sendgrid/sendgrid-nodejs
-const SENDGRID_API_KEY =
-  "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";
+const dotenv = require("dotenv");
 const sgMail = require("@sendgrid/mail");
-sgMail.setApiKey(SENDGRID_API_KEY);
+
+dotenv.config();
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 module.exports = function sendEmail(msg) {
   sgMail.send(msg);
